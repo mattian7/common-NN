deb54d9d6c718499fd4562dc38cd8caae2b63633
Matthew-creater <t2434704642@163.com>
2023-05-09 12:44:38 +0800

    second update MobileNet

--- model.py
+++ model.py
@@ -251,7 +251,6 @@ class DenseNet(nn.Module):
 
 
 class Block(nn.Module):
-    '''Depthwise conv + Pointwise conv'''
     def __init__(self, in_channels, out_channels, stride=1):
         super(Block, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, groups=in_channels, bias=False)
@@ -266,13 +265,10 @@ class Block(nn.Module):
 
 
 class MobileNet(nn.Module):
-    # (128,2) means conv planes=128, conv stride=2,
-    # by default conv stride=1
-    cfg = [64, (128,2), 128, (256,2), 256, (512,2),
-           512, 512, 512, 512, 512, (1024,2), 1024]
 
     def __init__(self, args, num_classes=10):
         super(MobileNet, self).__init__()
+        self.cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(32)
